e2476be032ce592840d016ec465404c94bfd3e87
Rafael Hiroshi <rafaelhkp@gmail.com>
2025-01-25 18:35:31 -0300

    Remove lifespan attribute

--- app/src/lambda_function.py
+++ app/src/lambda_function.py
@@ -9,13 +9,14 @@ DISCORD_PUBLIC_KEY = os.getenv("DISCORD_PUBLIC_KEY")
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
+
 app = Flask(__name__)
 asgi_app = WsgiToAsgi(app)
-handler = Mangum(asgi_app)
+handler = Mangum(asgi_app, lifespan="off")
 
 
 @app.route("/", methods=["POST"])
-async def interactions():
+def interactions():
     raw_request = request.json
     logger.info(raw_request)
     return interact(raw_request)
@@ -23,7 +24,7 @@ async def interactions():
 
 @verify_key_decorator(DISCORD_PUBLIC_KEY)
 def interact(raw_request):
-    if raw_request["type"] == 1:
+    if raw_request["type"] == 1:  # Ping interaction from Discord
         response_data = {"type": 1}
     else:
         data = raw_request["data"]
